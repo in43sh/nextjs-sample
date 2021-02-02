ae10098bcccf70654d6d796edca0c06e46460050
Sergey <in43sh@gmail.com>
2021-02-01 22:48:46 -0600

    Add MainLaout to posts.js file.

--- pages/posts.js
+++ pages/posts.js
@@ -1,12 +1,13 @@
 import Head from 'next/head';
+import { MainLayout } from '../components/MainLayout';
 
 export default function Posts() {
     return (
-        <>
+        <MainLayout>
             <Head>
                 <title>Posts Page | Next Course</title>
             </Head>
             <h1>Posts Page</h1>
-        </>
+        </MainLayout>
     )
 }
