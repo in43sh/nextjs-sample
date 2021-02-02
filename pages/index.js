b2e3f1ae73910fb2bda843520624e7bbd3dbd249
Sergey <in43sh@gmail.com>
2021-02-01 22:26:56 -0600

    Add meta tags to index.js file.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
-    return <>
-        <h1>Hellow Next.JS!</h1>
-        <p>
-            <Link href={"/about"}>
-                <a>About</a>
-            </Link>
-        </p>
-        <p><Link href={"/posts"}><a>Posts</a></Link></p>
-        <p>loremloremloremloremloremlorem</p>
-    </>
+    return (
+        <>
+            <Head>
+                <title>Next Title Index Hello</title>
+                <meta name="keywords" content="ключевые слова"/>
+                <meta name="description" content="this is youtube tutorial for next" />
+                {/* <meta name="charset" content="utf-8" /> */}
+                <meta charSet="utf-8" />
+            </Head>
+            <h1>Hellow Next.JS!</h1>
+            <p>
+                <Link href={"/about"}>
+                    <a>About</a>
+                </Link>
+            </p>
+            <p><Link href={"/posts"}><a>Posts</a></Link></p>
+            <p>loremloremloremloremloremlorem</p>
+        </>
+    )
 }
