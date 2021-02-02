aaa0072e5eef9c68711666b50680520b80a566be
Sergey <in43sh@gmail.com>
2021-02-02 00:24:09 -0600

    Add meta tags to MainLayout component.

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,12 +1,25 @@
-export function MainLayout({ children }) {
+import Link from 'next/link';
+import Head from 'next/head';
+
+export function MainLayout({ children, title = 'Next App' }) {
     return (
         <>
+            <Head>
+                <title>{title} | Next Course</title>
+                <meta name="keywords" content="ключевые слова"/>
+                <meta name="description" content="this is youtube tutorial for next" />
+                {/* <meta name="charset" content="utf-8" /> */}
+                <meta charSet="utf-8" />
+            </Head>
             <nav>
-                <h1>NAVIGATION</h1>
+                <Link href={"/"}><a>Home</a></Link>
+                <Link href={"/about"}><a>About</a></Link>
+                <Link href={"/posts"}><a>Posts</a></Link>
             </nav>
             <main>
                 {children}
             </main>
+            
         </>
     )
 }
