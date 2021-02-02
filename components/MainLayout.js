f26e186a2e0a6b62ef2932947e4a05612dd02e57
Sergey <in43sh@gmail.com>
2021-02-01 22:35:10 -0600

    Add content to MainLayout.js.

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,11 +1,11 @@
-export function MainLayout() {
+export function MainLayout({ children }) {
     return (
         <>
             <nav>
-
+                <h1>NAVIGATION</h1>
             </nav>
             <main>
-                
+                {children}
             </main>
         </>
     )
