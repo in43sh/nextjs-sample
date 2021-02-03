7bb75d09548dadee3052f1462da75a640addc394
Sergey <in43sh@gmail.com>
2021-02-03 11:37:47 -0600

    Add content to posts.js component.

--- pages/posts.js
+++ pages/posts.js
@@ -1,31 +1,45 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { MainLayout } from '../components/MainLayout';
 
-export default function Posts() {
-    const [posts, setPosts] = useState([]);
+export default function Posts({ posts }) {
+    // const [posts, setPosts] = useState([]);
 
-    useEffect(() =>  {
-        async function load() {
-            const response = await fetch('http://localhost:4200/posts');
-            const json = await response.json();
-            setPosts(json);
-        }
+    // useEffect(() =>  {
+    //     async function load() {
+    //         const response = await fetch('http://localhost:4200/posts');
+    //         const json = await response.json();
+    //         setPosts(json);
+    //     }
 
-        load();
-    }, [])
+    //     load();
+    // }, [])
     return (
         <MainLayout>
             <Head>
                 <title>Posts Page | Next Course</title>
             </Head>
             <h1>Posts Page</h1>
-            <pre>{JSON.stringify(posts, null, 2)}</pre>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link href={`/post/${post.id}`}>
+                            <a>{post.title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
         </MainLayout>
     )
 }
 
 Posts.getInitialProps = async () => {
-    const response = fetch('http://localhost:4200/posts');
-    const json = await response.json();
+    const response = await fetch('http://localhost:4200/posts');
+    const posts = await response.json();
+
+    return {
+        posts
+    }
 }
